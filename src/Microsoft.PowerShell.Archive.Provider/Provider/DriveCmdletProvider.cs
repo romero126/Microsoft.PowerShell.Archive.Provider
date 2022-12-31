2f5f6933c258a0d093b3c4b3bdae92e307035ea2
Juan Romero <romero126@gmail.com>
2022-12-31 15:04:16 -0800

    Initial Commit

---
+++ src/Microsoft.PowerShell.Archive.Provider/Provider/DriveCmdletProvider.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Management.Automation;
+using System.Management.Automation.Provider;
+using System.Linq;
+using System.Text;
+
+using Microsoft.PowerShell.Commands;
+
+namespace Microsoft.PowerShell.Archive
+{
+
+    public class ArchiveProvider :  NavigationCmdletProvider, IContentCmdletProvider
+    {
+        
+    }
+}
